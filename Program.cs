6cbdfdb54660333d1a08addbf5a2c23a93e5f0b7
devfelipeeduardo <felipeeduardofreitasm@gmail.com>
2025-11-14 18:16:40 -0300

    services(sistema_login): finalizando o sistema de cadastro, fiz a parte de listagem dos usuarios e seus produtos para facilitar futuro testes. Busquei utilizar inversão de dependencia para ajudar.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using Fase5_CalculadoraDeDescontoComLogin.Services;
+using Fase5_CalculadoraDeDescontoComLogin.Models.Entities;
+using Unity;
+using Fase5_CalculadoraDeDescontoComLogin.Interfaces;
+using System.ComponentModel;
+
+namespace Fase5.Calculadora
+{
+    class Program
+    {
+        public static Client Client { get; set; }
+        public static void Main(string[] args)
+        {
+            var registerContainer = RegisterDependencies(new UnityContainer());
+            IRegisterService registerService = registerContainer.Resolve<IRegisterService>();
+
+            Console.WriteLine("------------------");
+            Console.WriteLine("    Calculadora   ");
+            Console.WriteLine("    de Desconto   ");
+            Console.WriteLine("------------------");
+
+            while(true)
+            {
+                Console.WriteLine("Registre um usuário: ");
+                Console.Write("Digite seu login: ");
+                string login = Console.ReadLine();
+                Console.Write("Senha: ");
+                string password = Console.ReadLine();
+
+                Console.Write("Telefone: ");
+                string phoneNumber = Console.ReadLine();
+
+                Client = registerService.Registrar(login, password, phoneNumber);
+
+                Client.ShowClientData();
+            }
+        }
+
+        public static UnityContainer RegisterDependencies(UnityContainer unityContainer)
+        {
+            unityContainer.RegisterType<IRegisterService, RegisterService>();
+            return unityContainer;
+        }
+    }
+}
---
+++ Services/RegisterService.cs
@@ -0,0 +1,52 @@
+﻿using Fase5_CalculadoraDeDescontoComLogin.Interfaces;
+using Fase5_CalculadoraDeDescontoComLogin.Models.Entities;
+
+namespace Fase5_CalculadoraDeDescontoComLogin.Services
+{
+    internal class RegisterService : IRegisterService
+    {
+
+        public RegisterService() {}   
+        public Client Registrar(string login, string password, string phoneNumber)
+        {
+                if (login == null || login.Length == 0)
+                {
+                    Console.WriteLine($"O login: {login} não pode ser nulo ou igual a 0.");
+                }
+
+                if (login.Length < 6)
+                {
+                    Console.WriteLine($"O login: {login} deve ser maior que 6 dígitos");
+                }
+
+                if (login.Length > 15)
+                {
+                    Console.WriteLine($"O login {login} não pode ter mais de 15 caracteres");
+                }
+
+                //Senha
+                if (password == null || password.Length == 0)
+                {
+                    Console.WriteLine($"A senha não pode ser nula ou igual a 0.");
+                }
+
+                if (password.Length < 8)
+                {
+                    Console.WriteLine($"A senha precisa ser igual ou maior a 8 dígitos");
+                }
+
+                Thread.Sleep(2000);
+                Console.Clear();
+
+                string role = "usuario";
+
+                Client newUser = new Client { Login = login,
+                                                Password = password,
+                                                PhoneNumber = phoneNumber,
+                                                Role = role
+                };
+
+                return newUser;
+            }
+        }
+    }
