e1673e01bbee4253d5805747640ce6f6371ec071
Felipe Eduardo de Freitas Marques <felipe.marques@voxsolucoes.com.br>
2025-11-14 13:36:13 -0300

    initial: commit inicial

---
+++ Models/Interfaces/IProduto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fase5_CalculadoraDeDescontoComLogin.Models.Interfaces
+{
+    internal interface IProduto
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Description { get; set; }
+        string Brand {  get; set; }
+        int Price { get; set; }
+    }
+}
---
+++ Models/Interfaces/IUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fase5_CalculadoraDeDescontoComLogin.Models.Interfaces
+{
+    internal interface IUser
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Email { get; set; }
+        int PhoneNumber { get; set; }
+    }
+}
