aa2cf846bdaa44dc5f496c317e50c51bed42551b
devfelipeeduardo <felipeeduardofreitasm@gmail.com>
2025-11-20 21:35:40 -0300

    realocation(duplicate files): Existiam arquivos duplicados, devido nao atencao, segue correcao

--- Services/Interfaces/IRegisterUserService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Fase5_CalculadoraDeDescontoComLogin.Models.Entities;
-
-namespace Fase5_CalculadoraDeDescontoComLogin.Services.Interfaces
-{
-    public interface IRegisterUserService
-    {
-        Result<User> RegisterUser(string login, string password, string phoneNumber, List<User> usersRegistered);
-    }
-}
